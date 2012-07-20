385af8da246624743230ff382f7603059c32436a
ngandt-54 <ngandt_54@vnu.edu.vn>
2012-07-20 12:01:32 +0700

    Initial commit
    Signed-off-by: Ngan DT <ngandt_54@vnu.edu.vn>

---
+++ README.md
@@ -0,0 +1,4 @@
+vip2012-07_shopping-list
+========================
+
+An application to create a shopping list
---
+++ shopping-list/src/vip2012/g07/shoppinglist/Index.java
@@ -0,0 +1,13 @@
+package vip2012.g07.shoppinglist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Index extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
