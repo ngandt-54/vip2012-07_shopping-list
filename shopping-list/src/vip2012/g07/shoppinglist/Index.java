ad3f7564ef619bf2e4bd7e418715219e6ce2e8a0
Ngan DT <ngandt_54@vnu.edu.vn>
2012-07-20 16:16:09 +0700

    btn, spinner, textfield
    Signed-off-by: Ngan DT <ngandt_54@vnu.edu.vn>

--- shopping-list/src/vip2012/g07/shoppinglist/Index.java
+++ shopping-list/src/vip2012/g07/shoppinglist/Index.java
@@ -8,6 +8,10 @@ public class Index extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        
+//        setContentView(R.layout.main);
+        setContentView(R.layout.layout_add_new_products);
+        
+        
     }
 }
